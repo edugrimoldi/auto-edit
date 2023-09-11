cef06578c7fc9e3aaf5f75878be5b826e11c0620
karen leiva <karen.leivallejos@gmail.com>
2023-09-11 11:33:34 -0300

    add function csv

--- autoedit/interface/main_local.py
+++ autoedit/interface/main_local.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import datetime
 
 from colorama import Fore, Style
 import tensorflow as tf
@@ -12,28 +13,28 @@ from autoedit.ml_logic.model import *
 def process_data() -> tf.Tensor:
     """
     Take a balanced dataset of audios (1 sec. long) in .wav format.
-    This method transforms the audio-clips into STFT spectrograms (images) 
+    This method transforms the audio-clips into STFT spectrograms (images)
     and train a Convolutional Neural Network model.
     """
     print(Fore.MAGENTA + "\n⭐️ Preprocess and train ⭐️" + Style.RESET_ALL)
-    
+
     pos = tf.data.Dataset.list_files(POS+'/*.wav')
     neg = tf.data.Dataset.list_files(NEG+'/*.wav')
-    
+
     # Create column of 0 (negative) or 1 (positive)
-    positives = tf.data.Dataset.zip((pos, 
+    positives = tf.data.Dataset.zip((pos,
                                      tf.data.Dataset.from_tensor_slices(tf.ones(len(pos)))))
-    negatives = tf.data.Dataset.zip((neg, 
+    negatives = tf.data.Dataset.zip((neg,
                                      tf.data.Dataset.from_tensor_slices(tf.zeros(len(neg)))))
     data = positives.concatenate(negatives)
-    
+
     # Preprocess the data and create 16 batchs of N samples.
     data_map = data.map(preprocess_train)
     data_map = data_map.cache()
     data_map = data_map.shuffle(buffer_size=int(BUFFER_SIZE))
     data_map = data_map.batch(16)
     data_map = data_map.prefetch(8)
-    
+
     return data_map
 
 def train(data):
@@ -41,18 +42,18 @@ def train(data):
     # Create train and test datasets of test_size 0.3
     train = data.take(int(len(data)*0.7))
     test = data.skip(int(len(data)*0.7)).take(int(len(data)*0.3))
-    
+
     samples, labels = train.as_numpy_iterator().next()
     shape = samples.shape
-    
+
     # Train a model on the training set, using `model.py`
     model = initialize_model(input_shape=shape[1:])
-    
+
     patience = 8
-    
+
     model = compile_model(model)
-    model, history = train_model(model, 
-                                 train_data=train, 
+    model, history = train_model(model,
+                                 train_data=train,
                                  test_data=test,
                                  patience=patience,
                                  validation_data=test)
@@ -61,48 +62,88 @@ def train(data):
     #val_precision = np.max(history.history['val_precision'])
 
     print("✅ Train done")
-    
+
     save_model(model)
-    
-   
+
+
 def pred(video: bytes = None) -> pd.DataFrame:
     print(Fore.MAGENTA + "\n ⭐️ Use case: pred" + Style.RESET_ALL)
-    
+
     if video is None:
-        GAME = os.path.join('raw_data', 'Test', 'test5.wav')
+        GAME = os.path.join('raw_data', 'Test', 'demotest.wav')
         path = os.path.join(os.getcwd(), GAME)
         wav = load_wav_stereo(path)
-    
+
     else:
         wav = load_wav_stereo(video)
-    
-    print(type(wav))
+
     # Slice and preprocess the audio.
-    sample_slices = tf.keras.utils.timeseries_dataset_from_array(wav, 
-                                                                 wav, 
-                                                                 sequence_length=8000,         sequence_stride=8000, 
+    sample_slices = tf.keras.utils.timeseries_dataset_from_array(wav,
+                                                                 wav,
+                                                                 sequence_length=RATE_OUT,
+                                                                 sequence_stride=RATE_OUT,
                                                                  batch_size=1)
-    
-    sample_squeezed = tf.squeeze(sample_slices)
-    
-    sample_squeezed = sample_slices.map(preprocess_predict)
-    sample_squeezed = sample_squeezed.batch(64)
+
+    sample_slices = sample_slices.map(preprocess_predict)
+    sample_slices = sample_slices.batch(64)
 
     # Load saved model
     model = load_model()
-    
-    yhat = pred_model(model, sample_squeezed)
+
+    yhat = pred_model(model, sample_slices)
     yhat = [1 if prediction > 0.5 else 0 for prediction in yhat]
-    
+
+    print(f"✅ Prediction done")
+
     shoot_time = np.array((yhat, list(range(0, len(yhat)))))
     shoot_time= np.transpose(shoot_time)
-    
+
     # Create a dataframe with prediction results
     shoot_time_df = pd.DataFrame(shoot_time,
-                              columns=["pred","sec"])
-    
-    print(f"✅ Prediction done")
-    return shoot_time_df
+                                 columns=["pred","sec"])
+
+    clip_list = {}
+    set_start = 0
+    set_end = 0
+    counter = 0
+    shots_wanted = 2
+    step_size = 3
+
+    for index, row in shoot_time_df.iterrows():
+        if row['pred'] > 0:
+            if set_start == 0:
+                set_start = index
+                counter = 0
+            else:
+                set_end = index
+                counter = 0
+        else:
+            if counter == 0:
+                counter += 1
+            elif counter <= step_size and index < len(shoot_time_df) - 1:
+                counter += 1
+            else:
+                counter = 0
+                if set_start != 0:
+                    shot_count = shoot_time_df['pred'][set_start:set_end + 1].sum()
+                    if shot_count >= shots_wanted or index == (len(shoot_time_df)-1):
+                        clip_list[set_start] = set_end
+                    set_start = 0
+                    set_end = 0
+
+    if set_start != 0 and set_end != 0:
+        clip_list[set_start] = set_end
+
+    out_data = pd.DataFrame()
+    out_data['In'] = list(clip_list.keys())
+    out_data['Out'] = list(clip_list.values())
+
+    out_data['In']=out_data['In'].map(lambda x: str(datetime.timedelta(seconds=x)))
+    out_data['Out']=out_data['Out'].map(lambda x: str(datetime.timedelta(seconds=x)))
+    out_data.set_index('In', inplace=True)
+    out_data.to_csv('file.csv', header=None)
+
+    print(f"✅ File saved")
 
 
 if __name__ == '__main__':
--- autoedit/ml_logic/preprocessor.py
+++ autoedit/ml_logic/preprocessor.py
@@ -6,13 +6,13 @@ from autoedit.ml_logic.encoders import load_wav_stereo, separate_channels, creat
 from autoedit.params import *
 
 
-def preprocess_train(file_path: str, 
+def preprocess_train(file_path: str,
                 label: int) -> Tuple[tf.signal.stft, int]:
     print(Fore.BLUE + "\nPreprocessing audio..." + Style.RESET_ALL)
-    
+
     # Decode the audio file
     wav_total = load_wav_stereo(file_path)
-    
+
     wav_c1, wav_c2 = separate_channels(wav_total)
 
     # Fill with a padding of zeros and concatenate
@@ -20,17 +20,19 @@ def preprocess_train(file_path: str,
     zero_padding_c2 = tf.zeros(RATE_OUT - len(wav_c2), dtype=tf.float32)
     wav_c1 = tf.concat([zero_padding_c1, wav_c1],0)
     wav_c2 = tf.concat([zero_padding_c2, wav_c2],0)
-    
+
     # Create a STFT spectrogram for each channel and concatenate
     spectrogram = create_stft_spectrogram(wav_c1, wav_c2)
-    
+
     print("✅ Spectrogram created succesfully")
     return spectrogram, label
 
 
-def preprocess_predict(sample):
+def preprocess_predict(sample, index):
     print(Fore.BLUE + "\nPreprocessing audio..." + Style.RESET_ALL)
-        
+
+    sample= tf.squeeze(sample)
+
     wav_c1, wav_c2 = separate_channels(sample)
     spectrogram = create_stft_spectrogram(wav_c1, wav_c2)
 
