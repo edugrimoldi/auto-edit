579fa0c6cfac6dde98c8ec2ec0c960d0d9a3cbf3
maite-ai <mariafernandarios89@gmail.com>
2023-09-13 07:23:37 -0300

    API working with frontend

--- src/main.py
+++ src/main.py
@@ -1,12 +1,17 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware 
+from fastapi.responses import StreamingResponse
+
+from moviepy.editor import VideoFileClip
 
 from autoedit.interface.main_local import pred
 from autoedit.ml_logic.model import load_model
 
+import shutil
 import os
 
 app = FastAPI()
+
 # Allowing all middleware is optional, but good practice for dev purposes
 app.add_middleware(
     CORSMiddleware,
@@ -24,21 +29,29 @@ except Exception as e:
     print(f"❌ Error loading the model: {str(e)}")
 
 # http://127.0.0.1:8000/predict?video=video_number.mp4
-@app.get("/predict")
-def predict(file):
-
-    return os.path.join("..","file.csv")
-
+@app.post("/predict")
+async def predict(file: UploadFile = File(...)):
     """
     Make a single course prediction.
     """
+    locals_filename = "video_bonito.mp4"
+
+    with open(locals_filename, 'wb') as f:
+        shutil.copyfileobj(file.file, f)
+        
+    clip = VideoFileClip(locals_filename)
+    
+    #subclip mean video duration its from the place to start to the end
+    audio = clip.audio
+    audio.write_audiofile('temp-audio.wav')
+    
+    markers_df = pred('temp-audio.wav')
+    
+    return StreamingResponse
+
+    
     """
-    if hasattr(app.state, 'model') and app.state.model is not None:
-        markers_df = pred(file)
         #markers_df = markers_df.to_json()
-        return markers_df
-    else:
-        return {'error': 'Model not loaded.'}
 """
 
 @app.get("/")
